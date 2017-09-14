6503b68780ee6ee33dc8ec340530cfe7de42a688
lxp1413801 <lxp1413801@gmail.com>
2017-09-14 11:51:27 +0800

    lxpw

--- cs600-redo.X/src/api/sample.c
+++ cs600-redo.X/src/api/sample.c
@@ -8,15 +8,50 @@ uint8_t diffSampCount_D01=0;
 uint8_t diffSampCount_D23=0;
 uint8_t	diffSampCh=0;	
 
+int16_t __x_fliter(int16_t* buf,uint8_t len,uint8_t loop)
+{
+	int16_t max,min;
+	uint8_t i,maxLoc,minLoc;
+	int32_t t32=0;
+	if(len<3)return 0;
+	while(loop>0){
+		max=-32768;
+		min=32767;
+		t32=0;		
+		for(i=0;i<len;i++;){
+			if(min>buf[i]){min=buf[i];minLoc=i;}
+			if(max<buf[i]){max=buf[i];maxLoc=i;}
+			t32+=buf[i];
+		}
+		t32-=(max+min);
+		t32=t32/(len-2);
+		buf[minLoc]=t32;
+		buf[maxLoc]=t32;
+		loop--;
+	}
+	return (int16_t)t32;
+}
+
 uint8_t diff_pr_sample_fliter(void)
 {
 	int16_t max,min;
 	uint8_t i,loop,maxLoc,minLoc;
 	int32_t t32=0;
 	int16_t t16;
-	for()
-	
-	
+	for(loop=0;loop<4;loop++){
+		max=-32768;
+		min=32767;
+		t32=0;		
+		for(i=0;i<FAST_SAMPLE_FREQ/3;i++;){
+			if(min>sampBufDiffPr_D01[i]){min=sampBufDiffPr_D01[i];minLoc=i;}
+			if(max<sampBufDiffPr_D01[i]){max=sampBufDiffPr_D01[i];maxLoc=i;}
+			t32+=sampBufDiffPr_D01[i];
+		}
+		t32-=(max+min);
+		t32=t32/(FAST_SAMPLE_FREQ/3-2);
+		sampBufDiffPr_D01[minLoc]=t32;
+		sampBufDiffPr_D01[maxLoc]=t32;
+	}
 }
 
 uint8_t diff_pr_chip_sample_fast(void)
